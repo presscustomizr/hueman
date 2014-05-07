e16c8342cd7a1f3273c324a53156874580b4ecd7
Alexander Agnarson <alexander.agnarson@gmail.com>
2014-05-07 08:01:04 +0200

    Remove title separator if no description is set

--- functions.php
+++ functions.php
@@ -528,10 +528,13 @@ if ( ! function_exists( 'alx_wp_title' ) ) {
 		if ( is_feed() || class_exists('All_in_One_SEO_Pack') || class_exists('HeadSpace_Plugin') || class_exists('Platinum_SEO_Pack') || class_exists('wpSEO') || defined('WPSEO_VERSION') )
 			return $title;
 		if ( is_front_page() ) { 
-			$title = get_bloginfo('name').' - '.get_bloginfo('description'); 
+			$title = get_bloginfo('name').' - '.get_bloginfo('description');
+		}
+		if ( is_front_page() && get_bloginfo('description') == '' ) { 
+			$title = get_bloginfo('name');
 		}
 		if ( !is_front_page() ) { 
-			$title .= ' - '.get_bloginfo('name'); 
+			$title .= ' - '.get_bloginfo('name');
 		}
 		return $title;
 	}
