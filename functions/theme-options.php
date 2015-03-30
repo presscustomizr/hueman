137fbfc31f8ba67d27c572656069dfb3e24d3f07
Alexander Agnarson <alexander.agnarson@gmail.com>
2015-03-30 12:58:49 +0200

    Add additional Google fonts

--- functions/theme-options.php
+++ functions/theme-options.php
@@ -1006,7 +1006,7 @@ function custom_theme_options() {
 			'label'		=> 'Font',
 			'desc'		=> 'Select font for the theme',
 			'type'		=> 'select',
-			'std'		=> '30',
+			'std'		=> 'source-sans-pro',
 			'section'	=> 'styling',
 			'choices'	=> array(
 				array( 
@@ -1015,51 +1015,71 @@ function custom_theme_options() {
 				),
 				array( 
 					'value' => 'titillium-web-ext',
-					'label' => 'Titillium Web, Latin-Ext (Google Fonts)'
+					'label' => 'Titillium Web, Latin-Ext'
 				),
 				array( 
 					'value' => 'droid-serif',
-					'label' => 'Droid Serif, Latin (Google Fonts)'
+					'label' => 'Droid Serif, Latin'
 				),
 				array( 
 					'value' => 'source-sans-pro',
-					'label' => 'Source Sans Pro, Latin-Ext (Google Fonts)'
+					'label' => 'Source Sans Pro, Latin-Ext'
 				),
 				array( 
 					'value' => 'lato',
-					'label' => 'Lato, Latin (Google Fonts)'
+					'label' => 'Lato, Latin'
+				),
+				array( 
+					'value' => 'raleway',
+					'label' => 'Raleway, Latin'
 				),
 				array( 
 					'value' => 'ubuntu',
-					'label' => 'Ubuntu, Latin-Ext (Google Fonts)'
+					'label' => 'Ubuntu, Latin-Ext'
 				),
 				array( 
 					'value' => 'ubuntu-cyr',
-					'label' => 'Ubuntu, Latin / Cyrillic-Ext (Google Fonts)'
+					'label' => 'Ubuntu, Latin / Cyrillic-Ext'
 				),
 				array( 
 					'value' => 'roboto-condensed',
-					'label' => 'Roboto Condensed, Latin-Ext (Google Fonts)'
+					'label' => 'Roboto Condensed, Latin-Ext'
 				),
 				array( 
 					'value' => 'roboto-condensed-cyr',
-					'label' => 'Roboto Condensed, Latin / Cyrillic-Ext (Google Fonts)'
+					'label' => 'Roboto Condensed, Latin / Cyrillic-Ext'
+				),
+				array( 
+					'value' => 'roboto-slab',
+					'label' => 'Roboto Slab, Latin-Ext'
+				),
+				array( 
+					'value' => 'roboto-slab-cyr',
+					'label' => 'Roboto Slab, Latin / Cyrillic-Ext'
+				),
+				array( 
+					'value' => 'playfair-display',
+					'label' => 'Playfair Display, Latin-Ext'
+				),
+				array( 
+					'value' => 'playfair-display-cyr',
+					'label' => 'Playfair Display, Latin / Cyrillic'
 				),
 				array( 
 					'value' => 'open-sans',
-					'label' => 'Open Sans, Latin-Ext (Google Fonts)'
+					'label' => 'Open Sans, Latin-Ext'
 				),
 				array( 
 					'value' => 'open-sans-cyr',
-					'label' => 'Open Sans, Latin / Cyrillic-Ext (Google Fonts)'
+					'label' => 'Open Sans, Latin / Cyrillic-Ext'
 				),
 				array( 
 					'value' => 'pt-serif',
-					'label' => 'PT Serif, Latin-Ext (Google Fonts)'
+					'label' => 'PT Serif, Latin-Ext'
 				),
 				array( 
 					'value' => 'pt-serif-cyr',
-					'label' => 'PT Serif, Latin / Cyrillic-Ext (Google Fonts)'
+					'label' => 'PT Serif, Latin / Cyrillic-Ext'
 				),
 				array( 
 					'value' => 'arial',
@@ -1068,6 +1088,14 @@ function custom_theme_options() {
 				array( 
 					'value' => 'georgia',
 					'label' => 'Georgia'
+				),
+				array( 
+					'value' => 'verdana',
+					'label' => 'Verdana'
+				),
+				array( 
+					'value' => 'tahoma',
+					'label' => 'Tahoma'
 				)
 			)
 		),
