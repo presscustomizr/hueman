2a64ded6c776cb789191a4b6194235531382545e
Gabriel Hautclocq <ghautclocq@cgocean.com>
2015-08-25 23:38:02 +0200

    Added 5 optional widget zones + settings
    Added the following widget zones and their related settings in Blog
    theme options:
    - Before content
    - Before navigation links
    - Before related posts
    - Before comments section
    - After content

--- page.php
+++ page.php
@@ -1,30 +1,48 @@
 <?php get_header(); ?>
 
 <section class="content">
-	
+
 	<?php get_template_part('inc/page-title'); ?>
-	
+
+	<?php if ( ot_get_option('before-content-widget') == 'on' ): ?>
+	<div id="before-content-widget">
+		<?php dynamic_sidebar( 'before-content-widget' ); ?>
+	</div><!--/#before-content-widget-->
+	<?php endif; ?>
+
 	<div class="pad group">
-		
+
 		<?php while ( have_posts() ): the_post(); ?>
-		
+
 			<article <?php post_class('group'); ?>>
-				
+
 				<?php get_template_part('inc/page-image'); ?>
-				
+
 				<div class="entry themeform">
 					<?php the_content(); ?>
 					<div class="clear"></div>
 				</div><!--/.entry-->
-				
+
 			</article>
-			
+
+			<?php if ( ot_get_option('before-com-widget') == 'on' ): ?>
+			<div id="before-com-widget">
+				<?php dynamic_sidebar( 'before-com-widget' ); ?>
+			</div><!--/#before-com-widget-->
+			<?php endif; ?>
+
 			<?php if ( ot_get_option('page-comments') == 'on' ) { comments_template('/comments.php',true); } ?>
-			
+
 		<?php endwhile; ?>
-		
+
 	</div><!--/.pad-->
-	
+
+	<?php if ( ot_get_option('after-content-widget') == 'on' ): ?>
+	<div id="after-content-widget">
+		<?php dynamic_sidebar( 'after-content-widget' ); ?>
+	</div><!--/#after-content-widget-->
+	<?php endif; ?>
+
 </section><!--/.content-->
 
 <?php get_sidebar(); ?>
--- single.php
+++ single.php
@@ -1,39 +1,45 @@
 <?php get_header(); ?>
 
 <section class="content">
-	
+
 	<?php get_template_part('inc/page-title'); ?>
-	
+
+	<?php if ( ot_get_option('before-content-widget') == 'on' ): ?>
+	<div id="before-content-widget">
+		<?php dynamic_sidebar( 'before-content-widget' ); ?>
+	</div><!--/#before-content-widget-->
+	<?php endif; ?>
+
 	<div class="pad group">
-		
+
 		<?php while ( have_posts() ): the_post(); ?>
-			<article <?php post_class(); ?>>	
+			<article <?php post_class(); ?>>
 				<div class="post-inner group">
-					
+
 					<h1 class="post-title"><?php the_title(); ?></h1>
 					<p class="post-byline"><?php _e('by','hueman'); ?> <?php the_author_posts_link(); ?> &middot; <?php the_time(get_option('date_format')); ?></p>
-					
+
 					<?php if( get_post_format() ) { get_template_part('inc/post-formats'); } ?>
-					
+
 					<div class="clear"></div>
-					
-					<div class="entry <?php if ( ot_get_option('sharrre') != 'off' ) { echo 'share'; }; ?>">	
+
+					<div class="entry <?php if ( ot_get_option('sharrre') != 'off' ) { echo 'share'; }; ?>">
 						<div class="entry-inner">
 							<?php the_content(); ?>
 							<?php wp_link_pages(array('before'=>'<div class="post-pages">'.__('Pages:','hueman'),'after'=>'</div>')); ?>
 						</div>
 						<?php if ( ot_get_option('sharrre') != 'off' ) { get_template_part('inc/sharrre'); } ?>
-						<div class="clear"></div>				
+						<div class="clear"></div>
 					</div><!--/.entry-->
-					
-				</div><!--/.post-inner-->	
-			</article><!--/.post-->				
+
+				</div><!--/.post-inner-->
+			</article><!--/.post-->
 		<?php endwhile; ?>
-		
+
 		<div class="clear"></div>
-		
+
 		<?php the_tags('<p class="post-tags"><span>'.__('Tags:','hueman').'</span> ','','</p>'); ?>
-		
+
 		<?php if ( ( ot_get_option( 'author-bio' ) != 'off' ) && get_the_author_meta( 'description' ) ): ?>
 			<div class="author-bio">
 				<div class="bio-avatar"><?php echo get_avatar(get_the_author_meta('user_email'),'128'); ?></div>
@@ -42,15 +48,39 @@
 				<div class="clear"></div>
 			</div>
 		<?php endif; ?>
-		
+
+		<?php if ( ot_get_option('before-nav-widget') == 'on' ): ?>
+		<div id="before-nav-widget">
+			<?php dynamic_sidebar( 'before-nav-widget' ); ?>
+		</div><!--/#before-nav-widget-->
+		<?php endif; ?>
+
 		<?php if ( ot_get_option( 'post-nav' ) == 'content') { get_template_part('inc/post-nav'); } ?>
-		
+
+		<?php if ( ot_get_option('before-rel-widget') == 'on' ): ?>
+		<div id="before-rel-widget">
+			<?php dynamic_sidebar( 'before-rel-widget' ); ?>
+		</div><!--/#before-rel-widget-->
+		<?php endif; ?>
+
 		<?php if ( ot_get_option( 'related-posts' ) != '1' ) { get_template_part('inc/related-posts'); } ?>
-		
+
+		<?php if ( ot_get_option('before-com-widget') == 'on' ): ?>
+		<div id="before-com-widget">
+			<?php dynamic_sidebar( 'before-com-widget' ); ?>
+		</div><!--/#before-com-widget-->
+		<?php endif; ?>
+
 		<?php comments_template('/comments.php',true); ?>
-		
+
 	</div><!--/.pad-->
-	
+
+	<?php if ( ot_get_option('after-content-widget') == 'on' ): ?>
+	<div id="after-content-widget">
+		<?php dynamic_sidebar( 'after-content-widget' ); ?>
+	</div><!--/#after-content-widget-->
+	<?php endif; ?>
+
 </section><!--/.content-->
 
 <?php get_sidebar(); ?>
