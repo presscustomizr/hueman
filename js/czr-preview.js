71905368c01e1281cfbad7ace243beb39d28d6da
nikeo <nicolas@presscustomizr.com>
2016-05-29 15:06:37 +0200

    move preview callbacks server side part 2

--- js/czr-preview.js
+++ js/czr-preview.js
@@ -30,193 +30,128 @@
   //   });
   // });
 
+  if ( HUPreviewParams && ! HUPreviewParams.preview_ready_event_exists ) {
+    api.czr_preview = new api.CZR_preview();
+  }
+  else {
+    api.bind( 'preview-ready', function(){
+      api.czr_preview = new api.CZR_preview();
+    });
+  }
 
   api.CZR_preview = api.Class.extend( {
+        setting_cbs : {},
+        subsetting_cbs : {},//nested sub settings
+        _wp_sets : HUPreviewParams.wpBuiltinSettings || [],
+        _theme_options_name : HUPreviewParams.themeOptions,
+        initialize: function() {
+              var self = this;
+              //store the default control dependencies
+              this.setting_cbs = _.extend( self.setting_cbs, self.getSettingCbs() );
+              this.subsetting_cbs = _.extend( self.subsetting_cbs, self.getSubSettingCbs() );
+              this.syncData();
+              //api.trigger('czr-preview-ready');
+
+              this.addCbs();
+        },
+        getSettingCbs : function() { return {}; },
+        getSubSettingCbs : function() { return {}; },
+        syncData : function() {
+          //send infos to panel
+            api.preview.send( 'czr-wp-conditional-ready', api.settings.czr_wp_conditionals );
+            api.preview.send( 'houston-widget-settings',
+              _.extend( _wpWidgetCustomizerPreviewSettings, { availableWidgetLocations : _.values( api.settings.availableWidgetLocations ) } )
+            );
+            api.preview.send( 'czr-partial-refresh', ! _.isUndefined(_customizePartialRefreshExports) ? _customizePartialRefreshExports.partials : {} );
+
+            //TEST
+            //console.log('_wpCustomizeSettings', _wpCustomizeSettings, _wpCustomizeSettings.activeSections );
+            //console.log('_wpWidgetCustomizerPreviewSettings', _wpWidgetCustomizerPreviewSettings);
+            //console.log(' _customizePartialRefreshExports',  _customizePartialRefreshExports);
+            api.preview.send( 'czr-scopes-ready', _.has( _wpCustomizeSettings, 'czr_scopes' ) ? _wpCustomizeSettings.czr_scopes : '');
+        },
+
+        addCbs : function() {
+              var self = this;
+              _.each( self.setting_cbs, function( _cb, _setId ) {
+                    if ( ! api.has( self._build_setId(_setId) ) )
+                      return;
+                    api( self._build_setId(_setId) ).bind( self.setting_cbs[_setId] );
+              } );
+
+
+              //obj is : {
+              // set_id : this.id,
+              // model_id : obj.model.id,
+              // changed_prop : _changed,
+              // value : obj.model[_changed]
+              // }
+              api.preview.bind( 'sub_setting', function(obj) {
+                    //first get the "nude" option name
+                    var _opt_name = self._get_option_name( obj.set_id );
+
+                    //do we have custom callbacks for this subsetting ?
+                    if ( ! _.has(self.subsetting_cbs, _opt_name) )
+                      return;
+
+                    //do we have a custom callback for this model id ?
+                    if ( ! _.has( self.subsetting_cbs[_opt_name], obj.changed_prop ) )
+                      return;
+
+                    //execute the cb
+                    self.subsetting_cbs[_opt_name][obj.changed_prop]( obj );
+              });
+        },
+
+        /******************************************
+        * HELPERS
+        ******************************************/
+        /*
+        * @return string
+        * simple helper to build the setting id name if not a builtin wp setting id
+        */
+        _build_setId : function ( name ) {
+              var self = this;
+              //is wp built in ?
+              if ( _.contains( self._wp_sets, name ) )
+                return name;
+              //else
+              return -1 == name.indexOf( self._theme_options_name) ? [ self._theme_options_name + '[' , name  , ']' ].join('') : name;
+        },
+
+        _get_option_name : function(name) {
+              var self = this;
+              return name.replace(/\[|\]/g, '').replace(self._theme_options_name, '');
+        },
 
 
 
+        /*
+        * @return boolean
+        */
+        _is_external : function( _href  ) {
+              //EXT LINKS HELPERS
+              // var _url_comp     = (location.host).split('.'),
+              //   _nakedDomain  = new RegExp( _url_comp[1] + "." + _url_comp[2] );
+              //gets main domain and extension, no matter if it is a n level sub domain
+              //works also with localhost or numeric urls
+              var _thisHref = $.trim( _href ),
+                  _main_domain = (location.host).split('.').slice(-2).join('.'),
+                  _reg = new RegExp( _main_domain );
 
+              if ( _thisHref !== '' && _thisHref != '#' && _isValidURL( _thisHref ) )
+                return ! _reg.test( _thisHref );
+              return;
+        },
+
+        /*
+        * @return boolean
+        */
+        _isValidURL : function(_url){
+              //var _pattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+              var _pattern = /(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+              return _pattern.test( _url );
+        }
   });//api.Class.extend
-
-  var $_body    = $( 'body' ),
-      _wp_sets  = ['blogname', 'blogdescription', 'background_color'],
-      _setting_cbs = {},
-      _subsetting_cbs = {};//for nested sub settings
-
-  //Patch for wp versions before 4.1 => preview-ready signal isn't triggered
-  if ( HUPreviewParams && ! HUPreviewParams.preview_ready_event_exists )
-    $(document).ready(fireCzrPrev);
-  else
-    api.bind( 'preview-ready', function(){
-
-        //send infos to panel
-        api.preview.send( 'czr-wp-conditional-ready', api.settings.czr_wp_conditionals );
-        api.preview.send(
-          'houston-widget-settings',
-          _.extend( _wpWidgetCustomizerPreviewSettings, { availableWidgetLocations : _.values( api.settings.availableWidgetLocations ) } )
-        );
-        api.preview.send( 'czr-partial-refresh', ! _.isUndefined(_customizePartialRefreshExports) ? _customizePartialRefreshExports.partials : {} );
-
-        //TEST
-        //console.log('_wpCustomizeSettings', _wpCustomizeSettings, _wpCustomizeSettings.activeSections );
-        //console.log('_wpWidgetCustomizerPreviewSettings', _wpWidgetCustomizerPreviewSettings);
-        //console.log(' _customizePartialRefreshExports',  _customizePartialRefreshExports);
-        api.preview.send( 'czr-scopes-ready', _.has( _wpCustomizeSettings, 'czr_scopes' ) ? _wpCustomizeSettings.czr_scopes : '');
-
-
-        //settings cb
-        _.map( _setting_cbs, function( _cb, _setId ) {
-          if ( ! api.has( _build_setId(_setId) ) )
-            return;
-          api( _build_setId(_setId) ).bind( _setting_cbs[_setId] );
-        } );
-
-        //obj is : {
-        // set_id : this.id,
-        // model_id : obj.model.id,
-        // changed_prop : _changed,
-        // value : obj.model[_changed]
-        // }
-        api.preview.bind( 'sub_setting', function(obj) {
-          //first get the "nude" option name
-          var _opt_name = _get_option_name( obj.set_id );
-
-          //do we have custom callbacks for this subsetting ?
-          if ( ! _.has(_subsetting_cbs, _opt_name) )
-            return;
-
-          //do we have a custom callback for this model id ?
-          if ( ! _.has( _subsetting_cbs[_opt_name], obj.changed_prop ) )
-            return;
-
-          //execute the cb
-          _subsetting_cbs[_opt_name][obj.changed_prop]( obj );
-        });
-
-      }
-    );//api.bind('preview-ready')
-
-  /******************************************
-  * GLOBAL SETTINGS
-  ******************************************/
-  $.extend( _setting_cbs, {
-    blogname : function(to) {
-      var _hasLogo,
-          _logoSet = api.has( _build_setId('custom-logo') ) ? api( _build_setId('custom-logo') ).get() : '';
-
-      _hasLogo = ( _.isNumber(_logoSet) && _logoSet > 0 ) || ( ! _.isEmpty(_logoSet) && ( false !== _logoSet ) );
-
-      if ( _hasLogo )
-        return;
-      $( '.site-title a' ).text( to );
-    },
-    blogdescription : function(to) {
-      $( '.site-description' ).text( to );
-    },
-    'body-background' :  function(to) {
-      $('body').css('background-color', to);
-    },
-    'color-topbar' : function(to) {
-      $('.search-expand, #nav-topbar.nav-container, #nav-topbar .nav ul').css('background-color', to);
-    },
-    'color-header': function(to) {
-      $('#header').css('background-color', to);
-    },
-    'color-header-menu' : function(to) {
-      $('#nav-header.nav-container, #nav-header .nav ul').css('background-color', to);
-    },
-    'color-footer' : function(to) {
-      $('#footer-bottom').css('background-color', to);
-    },
-    credit : function(to) {
-      $( '#footer-bottom #credit' ).slideToggle();
-    },
-  });
-
-  /******************************************
-  * SUB SETTINGS
-  ******************************************/
-  $.extend( _subsetting_cbs, {
-    'social-links' : {
-        'title' : function( obj ) {
-          $( '[data-model-id="'+ obj.model_id +'"]', '.social-links' ).attr('title', obj.value );
-        },
-        'social-color' : function( obj ) {
-          $( '[data-model-id="'+ obj.model_id +'"]', '.social-links' ).css('color', obj.value );
-        },
-        'social-icon' : function( obj ) {
-          var $_el = $( '#'+ obj.model_id, '.social-links' ).find('i'),
-              _classes = ! _.isUndefined( $_el.attr('class') ) ? $_el.attr('class').split(' ') : [],
-              _prev = '';
-
-          //find the previous class
-          _.filter(_classes, function(_c){
-            if ( -1 != _c.indexOf('fa-') )
-              _prev = _c;
-          });
-
-          $( '[data-model-id="'+ obj.model_id +'"]', '.social-links' ).find('i').removeClass(_prev).addClass( obj.value );
-        },
-        'social-link' : function( obj ) {
-          $( '[data-model-id="'+ obj.model_id +'"]', '.social-links' ).attr('href', ! _isValidURL(obj.value) ? 'javascript:void(0);' : obj.value );
-        },
-        'social-target' : function( obj ) {
-          if ( 0 !== ( obj.value * 1 ) )
-            $( '[data-model-id="'+ obj.model_id +'"]', '.social-links' ).attr('target', "_blank");
-          else
-            $( '[data-model-id="'+ obj.model_id +'"]', '.social-links' ).removeAttr('target');
-
-        },
-    }
-  });
-
-
-
-  /******************************************
-  * HELPERS
-  ******************************************/
-  /*
-  * @return string
-  * simple helper to build the setting id name if not a builtin wp setting id
-  */
-  var _build_setId = function ( name ) {
-    //is wp built in ?
-    if ( _.contains(_wp_sets, name ) )
-      return name;
-    //else
-    return -1 == name.indexOf( 'hu_theme_options') ? [ 'hu_theme_options[' , name  , ']' ].join('') : name;
-  };
-
-  var _get_option_name = function(name) {
-    return name.replace(/\[|\]|hu_theme_options/g, '');
-  };
-
-  //EXT LINKS HELPERS
-  var _url_comp     = (location.host).split('.'),
-      _nakedDomain  = new RegExp( _url_comp[1] + "." + _url_comp[2] );
-
-  /*
-  * @return boolean
-  */
-  var _is_external = function( _href  ) {
-    //gets main domain and extension, no matter if it is a n level sub domain
-    //works also with localhost or numeric urls
-    var _thisHref = $.trim( _href ),
-        _main_domain = (location.host).split('.').slice(-2).join('.'),
-        _reg = new RegExp( _main_domain );
-
-    if ( _thisHref !== '' && _thisHref != '#' && _isValidURL( _thisHref ) )
-      return ! _reg.test( _thisHref );
-    return;
-  };
-
-  /*
-  * @return boolean
-  */
-  var _isValidURL = function(_url){
-    //var _pattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
-    var _pattern = /(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
-    return _pattern.test( _url );
-  };
 
 } )( wp.customize, jQuery, _ );
