571c4eb2563fe83cafb0ba505035fabea6e2c510
nikeo <nicolas@presscustomizr.com>
2016-10-06 12:06:48 +0200

    Made the partial refresh registration conditional with hu_is_partial_refreshed_on()

--- functions/czr/class-czr-init.php
+++ functions/czr/class-czr-init.php
@@ -146,9 +146,8 @@ if ( ! class_exists( 'HU_customize' ) ) :
 
     //hook : customize_register
     function hu_register_partials( WP_Customize_Manager $wp_customize ) {
-
-        // Abort if selective refresh is not available.
-        if ( ! isset( $wp_customize->selective_refresh ) ) {
+        //Bail if selective refresh is not available (old versions) or disabled (for skope for example)
+        if ( ! isset( $wp_customize->selective_refresh ) || ! hu_is_partial_refreshed_on() ) {
             return;
         }
 
