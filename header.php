3a0792ef421a2b46383b5a00bbe976e0877c295f
Dery Rahman A <dery.ra@gmail.com>
2016-10-20 15:28:53 +0700

    Add Index Follow meta tag

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="robots" content="index, follow">
   <link rel="profile" href="http://gmpg.org/xfn/11">
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
