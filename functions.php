4a20f38dfa23b1263815110ef2053e5a32253ba1
nikeo <nicolas@presscustomizr.com>
2016-10-25 18:33:55 +0200

    added tests code

--- functions.php
+++ functions.php
@@ -1,4 +1,7 @@
 <?php
+add_filter('ha_is_skop_on', '__return_false' );
+
+
 //Do not remove this
 load_template( get_template_directory() . '/functions/init-core.php' );
 
@@ -10,3 +13,57 @@ load_template( get_template_directory() . '/functions/init-core.php' );
 * https://codex.wordpress.org/Child_Themes
 *
 */
+
+add_action('__before_content', function() {
+  if ( ! is_customize_preview() )
+    return;
+    global $wp_customize;
+    ?>
+      <pre>
+        <?php print_r( 'CUSTOMIZED POSTED VALUES ?' ); ?>
+      </pre>
+    <?php
+    ?>
+      <pre>
+        <?php print_r($_POST); ?>
+      </pre>
+    <?php
+    ?>
+      <pre>
+        <?php if( function_exists('HU_AD') ) { print_r('IS CHANGESET ENABLED ? ' . HU_AD() -> ha_is_changeset_enabled() ); } ?>
+      </pre>
+    <?php
+      if ( $wp_customize -> changeset_post_id() ) {
+        $changeset_post = get_post( $wp_customize -> changeset_post_id() );
+        $changeset_data = json_decode( $changeset_post->post_content, true );
+      }
+
+    ?>
+      <pre>
+        <?php print_r('CHANGEET POST ID :' . $wp_customize -> changeset_post_id() ); ?>
+      </pre>
+
+      <div class="container">
+          <div class="raw-option">
+            <h2>CHANGESET POST DATA </h2>
+            <p>
+              <pre>
+                <?php if ( isset( $changeset_data ) ) print_r( $changeset_data ); ?>
+              </pre>
+            </p>
+            <h2>RAW OPTION COPYRIGHT </h2>
+            <p>
+              <pre>
+                <?php print_r( hu_get_raw_option( 'copyright' ) ); ?>
+              </pre>
+            </p>
+            <h2>RAW OPTION </h2>
+            <p>
+              <pre>
+                <?php //print_r( get_option('hu_theme_options') ); ?>
+              </pre>
+            </p>
+          </div>
+      </div>
+    <?php
+});
