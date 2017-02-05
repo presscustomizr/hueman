5442d8273707fd762ce6f4fcb4a7839759d86d4b
Rocco Aliberti <raliberti84@gmail.com>
2017-02-05 22:47:14 +0100

    Imp: woocommerce and wp-pagenavi code moved in plugins compat

--- functions/init-front.php
+++ functions/init-front.php
@@ -884,32 +884,6 @@ add_action( 'wp_footer', 'hu_ie_js_footer', 20 );
 
 
 
-/*  WooCommerce basic support
-/* ------------------------------------ */
-function hu_wc_wrapper_start() {
-  echo '<section class="content">';
-  echo '<div class="pad">';
-}
-function hu_wc_wrapper_end() {
-  echo '</div>';
-  echo '</section>';
-}
-remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
-remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
-add_action('woocommerce_before_main_content', 'hu_wc_wrapper_start', 10);
-add_action('woocommerce_after_main_content', 'hu_wc_wrapper_end', 10);
-
-
-/*  WP-PageNavi support - @devinsays (via GitHub)
-/* ------------------------------------ */
-function hu_deregister_styles() {
-  wp_deregister_style( 'wp-pagenavi' );
-}
-add_action( 'wp_print_styles', 'hu_deregister_styles', 100 );
-
-
-
-
 
 
 
