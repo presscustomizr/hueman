b2fe122d0ad176b629163eac446eef8b6145ac66
Philipp Schuster <phip1611@gmail.com>
2017-02-17 22:48:01 +0100

    improved "add to homescreen" (chrome/android)
    added support for meta-tag "mobile-web-app-capable" for improved "add to homescreen" (chrome/android)

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="profile" href="http://gmpg.org/xfn/11">
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+  <meta name="mobile-web-app-capable" content="yes">
 
   <?php wp_head(); ?>
 </head>
