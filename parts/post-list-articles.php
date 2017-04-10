09cc68065b28d4a00b51c78d3007db5620a0d7d6
eri-trabiccolo <raliberti84@gmail.com>
2017-04-10 14:52:09 +0200

    Imp: compatibility pro code for the post list articles template

--- parts/post-list-articles.php
+++ parts/post-list-articles.php
@@ -3,15 +3,20 @@
 /* ------------------------------------ */
 ?>
 <?php if ( hu_is_checked('blog-standard') ): ?>
-  <?php while ( have_posts() ): the_post(); ?>
-    <?php get_template_part('content-standard'); ?>
-  <?php endwhile; ?>
+  <div id="grid-wrapper" class="post-list-standard">
+    <?php while ( have_posts() ): the_post(); ?>
+      <?php get_template_part('content-standard'); ?>
+    <?php endwhile; ?>
+  </div>
 <?php else: ?>
   <div id="grid-wrapper" class="post-list group">
-    <?php global $wp_query; echo '<div class="post-row">'; while ( have_posts() ): the_post(); ?>
-      <?php get_template_part('content'); ?>
-    <?php if( ( $wp_query->current_post + 1 ) % 2 == 0 ) { echo '</div><div class="post-row">'; }; endwhile; echo '</div>'; ?>
+    <?php global $wp_query;
+      while ( have_posts() ): the_post();
+      if ( 0 == $wp_query->current_post || 0 == $wp_query->current_post % 2 ) { echo '<div class="post-row">'; }
+    ?>
+        <?php get_template_part('content'); ?>
+    <?php if( $wp_query->current_post == $wp_query -> post_count - 1  ||  ( $wp_query->current_post + 1 ) % 2 == 0 ) { echo '</div>'; }; endwhile; ?>
   </div><!--/.post-list-->
 <?php endif; ?>
 
-<?php get_template_part('parts/pagination'); ?>
+<?php hu_get_template_part('parts/pagination'); ?>
