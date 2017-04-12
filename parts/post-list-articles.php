5f10f18c12c4ff383b2538bae627123108b15a0a
eri-trabiccolo <raliberti84@gmail.com>
2017-04-12 12:04:39 +0200

    Imp: allow grid columns filtering

--- parts/post-list-articles.php
+++ parts/post-list-articles.php
@@ -9,13 +9,15 @@
     <?php endwhile; ?>
   </div>
 <?php else: ?>
-  <div id="grid-wrapper" class="post-list group">
-    <?php global $wp_query;
+  <div id="grid-wrapper" class="<?php echo implode( ' ', apply_filters('hu_classic_grid_wrapper_classes', array( 'post-list group') ) ) ?>">
+    <?php
+      $columns = apply_filters( 'hu_grid_columns', 2 );
+      global $wp_query;
       while ( have_posts() ): the_post();
-      if ( 0 == $wp_query->current_post || 0 == $wp_query->current_post % 2 ) { echo '<div class="post-row">'; }
+      if ( 0 == $wp_query->current_post || 0 == $wp_query->current_post % $columns ) { echo '<div class="post-row">'; }
     ?>
         <?php get_template_part('content'); ?>
-    <?php if( $wp_query->current_post == $wp_query -> post_count - 1  ||  ( $wp_query->current_post + 1 ) % 2 == 0 ) { echo '</div>'; }; endwhile; ?>
+    <?php if( $wp_query->current_post == $wp_query -> post_count - 1  ||  ( $wp_query->current_post + 1 ) % $columns == 0 ) { echo '</div>'; }; endwhile; ?>
   </div><!--/.post-list-->
 <?php endif; ?>
 
