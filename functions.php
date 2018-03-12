4ec2a618ad75a11988b2350279632151768975bf
Rocco Aliberti <raliberti84@gmail.com>
2018-03-12 16:54:54 +0100

    Imp: add early check on php version (min 5.3) and wp version (min 4.6) fixees #665

--- functions.php
+++ functions.php
@@ -1,4 +1,38 @@
 <?php
+if ( !defined( 'HU_MIN_PHP_VERSION' ) ) define ( 'HU_MIN_PHP_VERSION', 5.3 );
+if ( !defined( 'HU_MIN_WP_VERSION' ) ) define ( 'HU_MIN_WP_VERSION', 4.6 );
+
+if ( version_compare( phpversion(), HU_MIN_PHP_VERSION, '<' ) ) {
+    add_action( 'admin_notices'             , 'hu_display_min_php_message' );
+    return;
+}
+global $wp_version;
+if ( version_compare( $wp_version, HU_MIN_WP_VERSION, '<' ) ) {
+    add_action( 'admin_notices'             , 'hu_display_min_wp_message' );
+    return;
+}
+
+function hu_display_min_php_message() {
+    hu_display_min_requirement_notice( __( 'PHP', 'hueman' ), HU_MIN_PHP_VERSION );
+}
+
+function hu_display_min_wp_message() {
+    hu_display_min_requirement_notice( __( 'WordPress', 'hueman' ), HU_MIN_WP_VERSION );
+}
+
+
+function hu_display_min_requirement_notice( $requires_what, $requires_what_version ) {
+    $theme = wp_get_theme()->Name;
+    printf( '<div class="error"><p>%1$s</p></div>',
+        sprintf( __( 'The <strong>%1$s</strong> theme requires at least %2$s version %3$s', 'hueman' ),
+            $theme,
+            $requires_what,
+            $requires_what_version
+        )
+    );
+}
+
+
 //Do not remove this
 load_template( get_template_directory() . '/functions/init-core.php' );
 
