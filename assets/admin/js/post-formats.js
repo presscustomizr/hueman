29052ed7ced80e07492bbc981b701b3eed21b46e
Rocco Aliberti <raliberti84@gmail.com>
2018-04-19 16:01:34 +0200

    Fix: implement gutenberg post-format metaboxes visibility compatibility fixes #672

--- assets/admin/js/post-formats.js
+++ assets/admin/js/post-formats.js
@@ -14,31 +14,43 @@ jQuery(document).ready(function($) {
 		$('#format-audio,#format-aside,#format-chat,#format-gallery,#format-image,#format-link,#format-quote,#format-status,#format-video').hide();
 	}
 
+	var _wpPostFormatsInputSelectorClassical    = '#post-formats-select input[name="post_format"]',
+	    _wpPostFormatsInputSelectorGutenberg    = '.editor-post-format select',
+	    _isClassical                            = $(_wpPostFormatsInputSelectorClassical).length > 0,
+	    _isGutenberg                            = $(_wpPostFormatsInputSelectorGutenberg).length > 0;
+
+	if ( !( _isClassical || _isGutenberg ) ) {
+		return;
+	}
+
+	var _currentPostFormatSelector              = _isClassical ? _wpPostFormatsInputSelectorClassical + ':checked' : _wpPostFormatsInputSelectorGutenberg,
+	    _onChangePostFromatSelector             = _isClassical ? _wpPostFormatsInputSelectorClassical + ':radio'   : _wpPostFormatsInputSelectorGutenberg;
+
 	// Post Formats
-	if($("#post-formats-select").length) {
+
+	// Hide post format sections
+	hide_statuses();
+
+	// Supported post formats
+	var post_formats = ['audio','aside','chat','gallery','image','link','quote','status','video'];
+
+	// Get selected post format
+	var selected_post_format = $(_currentPostFormatSelector).val();
+
+	// Show post format meta box
+	if(jQuery.inArray(selected_post_format,post_formats) != '-1') {
+		$('#format-'+selected_post_format).show();
+	}
+
+	// Hide/show post format meta box when option changed
+	$(_onChangePostFromatSelector).change(function() {
 		// Hide post format sections
 		hide_statuses();
-
-		// Supported post formats
-		var post_formats = ['audio','aside','chat','gallery','image','link','quote','status','video'];
-
-		// Get selected post format
-		var selected_post_format = $("input[name='post_format']:checked").val();
-
-		// Show post format meta box
-		if(jQuery.inArray(selected_post_format,post_formats) != '-1') {
-			$('#format-'+selected_post_format).show();
+		// Shoe selected section
+		if(jQuery.inArray($(this).val(),post_formats) != '-1') {
+			$('#format-'+$(this).val()).show();
 		}
+	});
 
-		// Hide/show post format meta box when option changed
-		$("input[name='post_format']:radio").change(function() {
-			// Hide post format sections
-			hide_statuses();
-			// Shoe selected section
-			if(jQuery.inArray($(this).val(),post_formats) != '-1') {
-				$('#format-'+$(this).val()).show();
-			}
-		});
-	}
 
 });
