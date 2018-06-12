807b7c31c86a60a753ce5e4b952ca893caf7cf16
nikeo <nicolas@presscustomizr.com>
2018-06-12 14:48:46 +0200

    implemented the loading of the czr-base-fmk @after_setup_theme:15 implemented the loading of the social_links_module @after_setup_theme:20

--- functions/init-after-setup-theme.php
+++ functions/init-after-setup-theme.php
@@ -103,3 +103,54 @@ if ( ! function_exists( 'hu_setup' ) ) {
 
 }
 add_action( 'after_setup_theme', 'hu_setup' );
+
+add_action( 'after_setup_theme', 'hu_fn_load_czr_base_fmk', 15 );
+function hu_fn_load_czr_base_fmk() {
+    // load the czr-base-fmk
+    if ( ! isset( $GLOBALS['czr_base_fmk_namespace'] ) ) {
+        require_once(  dirname( __FILE__ ) . '/czr-base-fmk/czr-base-fmk.php' );
+        \hu_czr_fmk\CZR_Fmk_Base( array(
+           'text_domain' => 'customizr',
+           'base_url' => HU_BASE_URL . 'functions/czr-base-fmk',
+           'version' => HUEMAN_VER
+        ) );
+    } else {
+        //error_log('Warning => the czr_base_fmk should be loaded and instantiated by the theme.');
+    }
+}
+
+add_action( 'after_setup_theme', 'hu_fn_load_social_links_module', 20 );
+function hu_fn_load_social_links_module() {
+    // load the social links module
+    require_once( HU_BASE . 'functions/czr-modules/social-links/social_links_module.php' );
+    hu_register_social_links_module(
+        array(
+            'setting_id' => 'hu_theme_options[social-links]',
+
+            'base_url_path' => HU_BASE_URL . '/functions/czr-modules/social-links',
+            'version' => HUEMAN_VER,
+
+            'option_value' => hu_get_option( 'social-links' ), // for dynamic registration
+            'setting' => array(
+                'type' => 'option',
+                'default'  => array(),
+                'transport' => hu_is_partial_refreshed_on() ? 'postMessage' : 'refresh',
+                'sanitize_callback' => 'hu_sanitize_callback__czr_social_module',
+                'validate_callback' => 'hu_validate_callback__czr_social_module'
+            ),
+
+            'section' => array(
+                'id' => 'social_links_sec',
+                'title' => __( 'Social links', 'hueman' ),
+                'panel' => 'hu-general-panel',
+                'priority' => 30
+            ),
+
+            'control' => array(
+                'priority' => 10,
+                'label' => __( 'Create and organize your social links', 'hueman' ),
+                'type'  => 'czr_module',
+            )
+        )
+    );
+}
