3a32865f2b44e45f1ddfaf6987cc8a506328ac2c
Rocco Aliberti <raliberti84@gmail.com>
2018-08-01 09:49:22 +0200

    Fix: fix post-formats metaboxes gutenberg compatibility fixes #672

--- assets/admin/js/post-formats.js
+++ assets/admin/js/post-formats.js
@@ -8,49 +8,49 @@
 */
 
 jQuery(document).ready(function($) {
+	// temporary workaround to make sure gutenberg elements have been rendered, see https://github.com/presscustomizr/hueman/issues/672#issuecomment-409481887
+	setTimeout( function() {
+		// Hide post format sections
+		function hide_statuses() {
+			$('#format-audio,#format-aside,#format-chat,#format-gallery,#format-image,#format-link,#format-quote,#format-status,#format-video').hide();
+		}
 
-	// Hide post format sections
-	function hide_statuses() {
-		$('#format-audio,#format-aside,#format-chat,#format-gallery,#format-image,#format-link,#format-quote,#format-status,#format-video').hide();
-	}
+		var _wpPostFormatsInputSelectorClassical    = '#post-formats-select input[name="post_format"]',
+		    _wpPostFormatsInputSelectorGutenberg    = '.editor-post-format select',
+		    _isClassical                            = $(_wpPostFormatsInputSelectorClassical).length > 0,
+		    _isGutenberg                            = $(_wpPostFormatsInputSelectorGutenberg).length > 0;
 
-	var _wpPostFormatsInputSelectorClassical    = '#post-formats-select input[name="post_format"]',
-	    _wpPostFormatsInputSelectorGutenberg    = '.editor-post-format select',
-	    _isClassical                            = $(_wpPostFormatsInputSelectorClassical).length > 0,
-	    _isGutenberg                            = $(_wpPostFormatsInputSelectorGutenberg).length > 0;
+		if ( !( _isClassical || _isGutenberg ) ) {
+			return;
+		}
 
-	if ( !( _isClassical || _isGutenberg ) ) {
-		return;
-	}
+		var _currentPostFormatSelector              = _isClassical ? _wpPostFormatsInputSelectorClassical + ':checked' : _wpPostFormatsInputSelectorGutenberg,
+		    _onChangePostFromatSelector             = _isClassical ? _wpPostFormatsInputSelectorClassical + ':radio'   : _wpPostFormatsInputSelectorGutenberg;
 
-	var _currentPostFormatSelector              = _isClassical ? _wpPostFormatsInputSelectorClassical + ':checked' : _wpPostFormatsInputSelectorGutenberg,
-	    _onChangePostFromatSelector             = _isClassical ? _wpPostFormatsInputSelectorClassical + ':radio'   : _wpPostFormatsInputSelectorGutenberg;
+		// Post Formats
 
-	// Post Formats
-
-	// Hide post format sections
-	hide_statuses();
-
-	// Supported post formats
-	var post_formats = ['audio','aside','chat','gallery','image','link','quote','status','video'];
-
-	// Get selected post format
-	var selected_post_format = $(_currentPostFormatSelector).val();
-
-	// Show post format meta box
-	if(jQuery.inArray(selected_post_format,post_formats) != '-1') {
-		$('#format-'+selected_post_format).show();
-	}
-
-	// Hide/show post format meta box when option changed
-	$(_onChangePostFromatSelector).change(function() {
 		// Hide post format sections
 		hide_statuses();
-		// Shoe selected section
-		if(jQuery.inArray($(this).val(),post_formats) != '-1') {
-			$('#format-'+$(this).val()).show();
+
+		// Supported post formats
+		var post_formats = ['audio','aside','chat','gallery','image','link','quote','status','video'];
+
+		// Get selected post format
+		var selected_post_format = $(_currentPostFormatSelector).val();
+
+		// Show post format meta box
+		if(jQuery.inArray(selected_post_format,post_formats) != '-1') {
+			$('#format-'+selected_post_format).show();
 		}
-	});
 
-
+		// Hide/show post format meta box when option changed
+		$(_onChangePostFromatSelector).change(function() {
+			// Hide post format sections
+			hide_statuses();
+			// Shoe selected section
+			if(jQuery.inArray($(this).val(),post_formats) != '-1') {
+				$('#format-'+$(this).val()).show();
+			}
+		});
+	}, 300);
 });
