df2fc7c17ac3e8e6ebf4e02b88813a355fc690f1
nikeo <nicolas@presscustomizr.com>
2019-12-02 16:42:37 +0100

    updated Nimble Builder recommendation notification in admin

--- functions/admin/class-plugin-rec.php
+++ functions/admin/class-plugin-rec.php
@@ -3,7 +3,10 @@
 function hu_rec_notice_is_dismissed() {
   $dismissed = get_user_meta( get_current_user_id(), 'dismissed_wp_pointers', true );
   $dismissed_array = array_filter( explode( ',', (string) $dismissed ) );
-  return in_array( REC_NOTICE_ID, $dismissed_array );
+  // always display in dev mode when Nimble is not activated yet
+  if ( defined('CZR_DEV') && true === CZR_DEV )
+    return false;
+  return ( defined('NIMBLE_RECOMMENDATION_OFF') && true === NIMBLE_RECOMMENDATION_OFF ) || in_array( REC_NOTICE_ID, $dismissed_array );
 }
 
 add_action( 'admin_notices', 'hu_maybe_render_rec_notice' );
@@ -20,19 +23,21 @@ function hu_maybe_render_rec_notice() {
   $is_nimble_installed = isset( $installed_plugins[ $plugin ] );
 
   $heading = sprintf( __('Hueman theme recommends %1$s.', 'hueman' ),
-          sprintf('<a href="%1$s" class="thickbox">%2$s</a>',
+          sprintf('<a href="%1$s" class="thickbox" target="_blank">%2$s</a>',
               wp_nonce_url( 'plugin-install.php?tab=plugin-information&amp;plugin=nimble-builder&amp;TB_iframe=true&amp;width=640&amp;height=500'),
               __('Nimble Page Builder', 'hueman')
           )
       );
 
-  $message = sprintf( '<span style="font-weight:normal; font-style:italic">%1$s<br/> %2$s<br/>%3$s</span>',
-    __( 'Nimble Page Builder is a free, easy to use yet powerful page builder used by 40K+ WordPress websites.',  'hueman'),
+  $message = sprintf( '<span style="font-weight:normal;">%1$s<br/> %2$s<br/>%3$s<br/>%4$s</span>',
+    __( 'Developers of the Hueman theme have created Nimble Builder, a free, powerful yet easy-to-use page builder already active on 40K+ WordPress websites.',  'hueman'),
+    __( 'It allows you to drag and drop mobile-ready sections on <i>really</i> any page of your site, including home, posts, pages, products, archives, 404, search pages, ...', 'hueman' ),
     sprintf(
-        __( 'It allows you to create mobile ready column layouts, and to drag-and-drop modules like post grids, buttons, widget zones, maps, icons, or beautiful pre-built sections with engaging %1$s, in any page of your site. Nimble Builder uses the live customizer which is the native WordPress interface for real-time design.', 'hueman' ),
-        sprintf('<a href="%1$s" target="_blank" title="%2$s">%2$s</a>', esc_url('demo-hueman.presscustomizr.com/nimble-builder/'), __('parallax backgrounds', 'hueman') )
+        __( 'You can insert simple text zones, but also create %1$s, insert post grids, column structures, buttons, widget zones, maps, icons, and much more, or use pre-designed sections with professional %2$s.', 'hueman'),
+        sprintf('<a href="%1$s" target="_blank" title="%2$s">%2$s</a>', esc_url('nimblebuilder.com/mp4-video-background-with-delay/'), __('video backgrounds', 'hueman') ),
+        sprintf('<a href="%1$s" target="_blank" title="%2$s">%2$s</a>', esc_url('demo-hueman.presscustomizr.com/nimble-builder'), __('parallax effect', 'hueman') )
     ),
-    __( "The plugin has been designed to integrate perfectly with Hueman theme. Lightweight and safe.", 'hueman')
+    __( "The plugin is lightweight and has been designed to integrate seamlessly with Hueman and any WordPress theme.", 'hueman')
   );
 
   if ( $is_nimble_installed ) {
@@ -67,7 +72,7 @@ function hu_maybe_render_rec_notice() {
         <p><?php echo $message; ?></p>
         <span class="czr-rec-button"><a class="button button-primary button-hero activate-now" href="<?php echo esc_attr( $button_link ); ?>" data-name="Nimble Builder" data-slug="nimble-builder"><?php echo $button_text; ?></a></span>
       </div>
-      <div class="czr-tgmpa-img-block"><img src="<?php echo get_template_directory_uri() . '/assets/admin/img/nimble_and_hueman_145.gif'; ?>" alt="Nimble Builder" title="Nimble Builder" class="czr-nimble-img"></div>
+      <div class="czr-tgmpa-img-block"><img src="https://4f79e5dd0a8bab774639-1217c3223db807f4a8bb54bb3dca6912.ssl.cf5.rackcdn.com/img/nimble_and_hueman_145.gif" alt="Nimble Builder" title="Nimble Builder" class="czr-nimble-img"></div>
     </div>
   </div>
   <?php
