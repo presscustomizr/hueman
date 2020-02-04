b4a478b5f7a7424357153f4257d805f294ed67ac
nikeo <nicolas@presscustomizr.com>
2020-02-04 12:40:13 +0100

    fixed : [html] Bad value for attribute datetime on element time as per HTML specs : the literal did not satisfy the time-datetime format.

--- parts/single-author-date.php
+++ parts/single-author-date.php
@@ -9,6 +9,7 @@ if ( ! ( $display_author || $display_date ) ) {
     return;
 }
 $published_date = get_the_date( get_option('date_format') );
+$machine_readable_published_date = esc_attr( get_the_date( 'c' ) );
 ?>
 <p class="post-byline">
 <?php if ( $display_author ) :?>
@@ -22,10 +23,15 @@ $published_date = get_the_date( get_option('date_format') );
 <?php if ( $display_date ) :?>
     <?php if ( hu_is_checked('structured-data') ) : ?>
           <?php if( get_the_modified_date() != get_the_date() || get_the_modified_time() != get_the_time() ) : ?>
-              <?php _e('Published', 'hueman'); ?> <time class="published" datetime="<?php echo $published_date; ?>"><?php echo $published_date; ?></time>
-              &middot; <?php _e('Updated', 'hueman'); ?> <time class="updated" datetime="<?php the_modified_date( get_option('date_format') ); ?>"><?php the_modified_date( get_option('date_format') ); ?></time>
+
+              <?php
+                $modified_date = get_the_modified_date( get_option('date_format') );
+                $machine_readable_modified_date = esc_attr( get_the_modified_date( 'c' ) );
+              ?>
+              <?php _e('Published', 'hueman'); ?> <time class="published" datetime="<?php echo $machine_readable_published_date; ?>"><?php echo $published_date; ?></time>
+              &middot; <?php _e('Updated', 'hueman'); ?> <time class="updated" datetime="<?php echo $machine_readable_modified_date; ?>"><?php echo $modified_date; ?></time>
           <?php else : ?>
-              <time class="published" datetime="<?php echo $published_date; ?>"><?php echo $published_date; ?></time>
+              <time class="published" datetime="<?php echo $machine_readable_published_date; ?>"><?php echo $published_date; ?></time>
           <?php endif; ?>
     <?php else : ?>
         <span class="published"><?php echo $published_date; ?></span>
