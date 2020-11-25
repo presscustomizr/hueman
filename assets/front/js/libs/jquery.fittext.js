45fa13e723de346cc5eef31d9998e1e00e42b2a9
nikeo <nicolas@presscustomizr.com>
2020-11-25 10:53:09 +0100

    fixed : [WP5.7] fixed wrong usage $.fn.css() method

--- assets/front/js/libs/jquery.fittext.js
+++ assets/front/js/libs/jquery.fittext.js
@@ -27,7 +27,7 @@
 
       // Resizer() resizes items based on the object width divided by the compressor * 10
       var resizer = function () {
-        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize))  + 'px');
       };
 
       // Call once to set.
