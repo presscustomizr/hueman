752b76e772a1ba65dcf0e568dcba6f7ac6165234
nikeo <nicolas@presscustomizr.com>
2021-06-21 14:27:22 +0200

    fixed : PHP Notice: Trying to access array offset on value of type bool. fixes #964

--- functions/init-functions.php
+++ functions/init-functions.php
@@ -499,9 +499,11 @@ function hu_get_img_source( $img ) {
     if ( is_numeric( $img ) ) {
         $_attachment_id     = $img;
         $_attachment_data   = apply_filters( "hu_attachment_img" , wp_get_attachment_image_src( $_attachment_id, 'full' ), $_attachment_id );
-        $_img_src           = $_attachment_data[0];
-        $_width             = ( isset($_attachment_data[1]) && $_attachment_data[1] > 1 ) ? $_attachment_data[1] : $_width;
-        $_height            = ( isset($_attachment_data[2]) && $_attachment_data[2] > 1 ) ? $_attachment_data[2] : $_height;
+        if ( is_array($_attachment_data) ) {
+          $_img_src           = $_attachment_data[0];
+          $_width             = ( isset($_attachment_data[1]) && $_attachment_data[1] > 1 ) ? $_attachment_data[1] : $_width;
+          $_height            = ( isset($_attachment_data[2]) && $_attachment_data[2] > 1 ) ? $_attachment_data[2] : $_height;
+        }
     } else { //old treatment
         //rebuild the img path : check if the full path is already saved in DB. If not, then rebuild it.
         $upload_dir         = wp_upload_dir();
